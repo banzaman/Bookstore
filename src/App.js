766dcf37d3547faee83d885cd928f922b9604b2c
MARK RUBANZA <markrubanza10@gmail.com>
2023-02-24 18:56:31 +0300

    Initialize components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Navigation from './components/Navigation';
 
-function App() {
-  return 
-}
+const App = () => (
+  <section>
+    <Router>
+      <Navigation />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
+  </section>
+);
 
 export default App;
